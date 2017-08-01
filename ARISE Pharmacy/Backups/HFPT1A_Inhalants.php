41512b00c3bb5500e87edec3324882b103ff99df
gmcedarblade <gmcedarblade@gmail.com>
2017-08-01 15:05:40 -0500

    first commit

---
+++ ARISE Pharmacy/Backups/HFPT1A_Inhalants.php
@@ -0,0 +1,313 @@
+<?php
+
+session_start();
+
+/*
+ * Use this to test what is stored
+ * in the session used for the scenarios.
+ * Uncomment when needed.
+ */
+//if(isset($_SESSION['savedDrugs'])) {
+//    var_dump($_SESSION['savedDrugs']);
+//}
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>Inhalants Page HFPT1A</title>
+    <link rel="stylesheet" type="text/css" href="https://www.wisc-online.com/ARISE_Files/CSS/AriseMainCSS.css?random=pasadsdsds25235h">
+</head>
+<style>
+    button {
+        float: none;
+    }
+</style>
+<body style="text-align: center;" id="body">
+<p style="text-align: center;">Tap on the drugs you wish to select and when done selecting click "Add" to add to order</p>
+<img src="https://www.wisc-online.com/ARISE_Files/Experimental/Hot%20Spot/Pharm_Tech/Pharmacy-Shelves-Example.png" width="100%" class="map" usemap="#features" id="drugShelfImage">
+<map name="features" id="map">
+    <area id="drug0"  shape="rect" coords="0,0,241,281">
+    <area id="drug1"  shape="rect" coords="241,0,482,281">
+    <area id="drug2"  shape="rect" coords="482,0,723,281">
+    <area id="drug3"  shape="rect" coords="723,0,964,281">
+    <area id="drug4"  shape="rect" coords="0,281,241,562">
+    <area id="drug5"  shape="rect" coords="241,281,482,562">
+    <area id="drug6"  shape="rect" coords="482,281,723,562">
+    <area id="drug7"  shape="rect" coords="723,281,964,562">
+    <area id="drug8"  shape="rect" coords="0,562,241,843">
+    <area id="drug9"  shape="rect" coords="241,562,482,843">
+    <area id="drug10" shape="rect" coords="482,562,723,843">
+    <area id="drug11" shape="rect" coords="723,562,964,843">
+    <area id="drug12" shape="rect" coords="0,843,241,1124">
+    <area id="drug13" shape="rect" coords="241,843,482,1124">
+    <area id="drug14" shape="rect" coords="482,843,723,1124">
+    <area id="drug15" shape="rect" coords="723,843,964,1124">
+</map>
+<div style="height: 20px;">
+    <button class="submit" id="clear">Clear Selections</button>
+    <button type="button" class="submit" id="add" name="Add" value="Add">Add</button>
+    <button class="submit" id="dailyMed" onclick="location.href='https://dailymed.nlm.nih.gov/dailymed/';">Medication Reference Website</button>
+    <button class="submit" id="home">Home</button>
+</div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://www.wisc-online.com/ARISE_Files/JS/fastClickJS.js"></script>
+<script type="text/javascript">
+
+    var body = document.querySelector('#body');
+    var map = document.querySelector('#map');
+
+    var clearButton = document.querySelector('#clear');
+    var addButton = document.querySelector('#add');
+    var dailyMed = document.querySelector('#dailyMed');
+
+    var shelf = document.querySelector('#drugShelfImage');
+    var shelfOffSetTop = shelf.offsetTop;
+
+    const DRUG_WIDTH = shelf.width/4;
+    const DRUG_HEIGHT = 281.25;
+    const COMMA = ',';
+
+    var backgroundOff = true;
+
+    for(var i = 0; i < 16; i++) {
+
+        var drug = document.querySelector('#drug' + i)
+
+        drug.addEventListener('click', function(e){
+
+            e.preventDefault();
+
+            if (backgroundOff) {
+
+                if (this.id === 'drug0' || this.id === 'drug1' || this.id === 'drug2' || this.id === 'drug3'){
+
+                    var coords = this.getAttribute('coords');
+                    var coordSplit = coords.split(COMMA);
+                    const ROW_ONE_X = coordSplit[0];
+                    const ROW_ONE_Y = coordSplit[1] + shelfOffSetTop;
+
+                    addOverlay(this.id, ROW_ONE_X, ROW_ONE_Y, DRUG_WIDTH, DRUG_HEIGHT);
+
+                } else if (this.id === 'drug4' || this.id === 'drug5' || this.id === 'drug6' || this.id === 'drug7') {
+
+                    var coords = this.getAttribute('coords');
+                    var coordSplit = coords.split(COMMA);
+
+                    const ROW_TWO_X = coordSplit[0];
+                    const ROW_TWO_Y = DRUG_HEIGHT + shelfOffSetTop;
+
+                    addOverlay(this.id, ROW_TWO_X, ROW_TWO_Y, DRUG_WIDTH, DRUG_HEIGHT);
+
+                } else if (this.id === 'drug8' || this.id === 'drug9' || this.id === 'drug10' || this.id === 'drug11') {
+
+                    var coords = this.getAttribute('coords');
+                    var coordSplit = coords.split(COMMA);
+
+                    const ROW_THREE_X = coordSplit[0];
+                    const ROW_THREE_Y = (DRUG_HEIGHT * 2) + shelfOffSetTop;
+
+                    addOverlay(this.id, ROW_THREE_X, ROW_THREE_Y, DRUG_WIDTH, DRUG_HEIGHT);
+
+                } else if (this.id === 'drug12' || this.id === 'drug13' || this.id === 'drug14' || this.id === 'drug15') {
+
+                    var coords = this.getAttribute('coords');
+                    var coordSplit = coords.split(COMMA);
+
+                    const ROW_FOUR_X = coordSplit[0];
+                    const ROW_FOUR_Y = (DRUG_HEIGHT * 3) + shelfOffSetTop;
+
+                    addOverlay(this.id, ROW_FOUR_X, ROW_FOUR_Y, DRUG_WIDTH, DRUG_HEIGHT);
+                }
+
+            }
+
+        }, false);
+
+    }
+
+    function addOverlay(drug, x, y, width, height) {
+
+        var overlay = document.createElement('div');
+
+        overlay.id = drug;
+
+        overlay.style.backgroundColor = "rgba(0, 150, 0, 0)";
+        overlay.style.position = "absolute";
+        overlay.style.left = x + 'px';
+        overlay.style.top = y + 'px';
+        overlay.style.width = width + 'px';
+        overlay.style.height = height + 'px';
+
+        overlay.dataset.overlayOn = true;
+
+        overlay.addEventListener('click', function(e){
+
+            e.preventDefault();
+            overlay.dataset.overlayon = false;
+            map.removeChild(overlay);
+
+        }, false);
+
+        map.appendChild(overlay);
+
+        var checkMark = document.createElement('div');
+
+        checkMark.style.backgroundImage = 'url("https://www.wisc-online.com/ARISE_Files/Experimental/Hot%20Spot/Pharm_Tech/check-mark_03.png")';
+        checkMark.style.backgroundRepeat = 'no-repeat';
+        checkMark.style.float = 'right';
+        checkMark.style.width = 57 + 'px';
+        checkMark.style.height = 50 + 'px';
+
+        overlay.appendChild(checkMark);
+
+    }
+
+    clearButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        var list = map.querySelectorAll('[data-overlay-on="true"]');
+        for(var i = 0; i < list.length; i++){
+            map.removeChild(list[i]);
+        }
+        backgroundOff = true;
+
+    }, false);
+
+    addButton.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        var activeList = map.querySelectorAll('[data-overlay-on="true"]');
+
+        var drugList = {drug8: "Albuterol Sulfate",
+                drug11: "Spiriva",
+                drug14: "Pulmicort"};
+
+        var drugListCount = (Object.keys(drugList)).length;
+
+        var arraysAreEqual = true;
+
+        if(activeList.length == drugListCount) {
+
+            for (var i = 0; i < activeList.length; i++) {
+
+                if (!drugList.hasOwnProperty('' + activeList[i].id)) {
+                    arraysAreEqual = false;
+                    break;
+                }
+            }
+
+            if(arraysAreEqual == true) {
+
+                $.ajax({
+                    type: "GET",
+                    data: {myData: "true"},
+                    success: function () {
+
+                        $confirmationPopOver = $('<div></div>');
+                        $('#map').append($confirmationPopOver);
+                        $confirmationPopOver.text("You have added the selected medication(s) to the order.");
+                        $confirmationPopOver.width(625).height(80).css({
+                            backgroundColor: "white",
+                            position: "absolute",
+                            left: "170px",
+                            top: "525px",
+                            fontSize: "38px",
+                            padding: "20px",
+                            textAlign: "center"
+                        }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+
+                    },
+                    error: function (e) {
+                        console.log("Error: " + e.message);
+
+                    }
+
+                });
+
+            } else {
+
+
+                $.ajax({
+                    type: "GET",
+                    data: {myData: "false"},
+                    success: function () {
+
+                        $incorrectPopOver = $('<div></div>');
+                        $('#map').append($incorrectPopOver);
+                        $incorrectPopOver.text("You have selected one or more incorrect medications to add to the order. Please try again.");
+                        $incorrectPopOver.width(625).height(125).css({
+                            backgroundColor: "white",
+                            position: "absolute",
+                            left: "170px",
+                            top: "525px",
+                            fontSize: "38px",
+                            padding: "20px",
+                            textAlign: "center"
+                        }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+
+                    },
+                    error: function (e) {
+                        console.log("Error: " + e.message);
+
+                    }
+
+                });
+            }
+
+        } else {
+
+            $.ajax({
+                type: "GET",
+                data: {myData: "false"},
+                success: function () {
+
+                    $incorrectPopOver = $('<div></div>');
+                    $('#map').append($incorrectPopOver);
+                    $incorrectPopOver.text("You have selected an incorrect amount of medications. Please try again.");
+                    $incorrectPopOver.width(625).height(125).css({
+                        backgroundColor: "white",
+                        position: "absolute",
+                        left: "170px",
+                        top: "525px",
+                        fontSize: "38px",
+                        padding: "20px",
+                        textAlign: "center"
+                    }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+
+                },
+                error: function (e) {
+                    console.log("Error: " + e.message);
+
+                }
+
+            });
+
+        }
+
+
+    }, false);
+
+</script>
+<?php
+
+if(isset($_GET['myData'])) {
+    $_GET['myData'] = json_decode($_GET['myData']);
+
+    $_SESSION['savedDrugs']['inhalants'] = $_GET['myData'];
+
+}
+?>
+<script type="text/javascript">
+    var ARIS = {};
+    ARIS.ready = function() {
+
+        document.getElementById("home").onclick = function(){
+            ARIS.exitToWebpage(26084);
+        };
+
+    }
+</script>
+</html>
---
+++ ARISE Pharmacy/Ryan_Shelves/PharmTechHome_HFPT1A.php
@@ -0,0 +1,333 @@
+<?php
+if(!isset($_SESSION)) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>ARISE Pharmacy Home</title>
+    <link rel="stylesheet" type="text/css" href="https://www.wisc-online.com/ARISE_Files/CSS/AriseMainCSS.css?random=pasadsdsds25235h">
+    <style>
+        button {
+            float: none;
+        }
+        .shelf {
+
+        }
+
+        #tabAF {
+            position: absolute;
+            top: 786px;
+            left: 8px;
+            width: 165px;
+            height: 347px;
+            -webkit-clip-path:polygon(0 31px, 91px 31px, 165px 0px, 165px 269px, 91px 347px, 0px 347px);
+            clip-path: polygon(0 31px, 91px 31px, 165px 0px, 165px 269px, 91px 347px, 0px 347px);
+        }
+
+        #tabGL {
+            position: absolute;
+            top: 497px;
+            left: 8px;
+            width: 165px;
+            height: 318px;
+            -webkit-clip-path:polygon(0 0, 91px 0, 165px 29px, 165px 288px, 91px 318px, 0px 318px);
+        }
+
+        #tabMR {
+            position: absolute;
+            top: 169px;
+            left: 8px;
+            width: 165px;
+            height: 354px;
+            -webkit-clip-path:polygon(0 0, 91px 0, 165px 75px, 165px 354px, 91px 326px, 0px 326px);
+        }
+
+        #tabSZ {
+            position: absolute;
+            top: 8px;
+            left: 8px;
+            width: 348px;
+            height: 167px;
+            -webkit-clip-path:polygon(0 0, 318px 0, 318px 91px, 348px 167px, 79px 167px, 0px 91px);
+        }
+
+        #liquids {
+            position: absolute;
+            top: 8px;
+            left: 329px;
+            width: 348px;
+            height: 167px;
+            -webkit-clip-path:polygon(0 0, 318px 0, 318px 91px, 289px 167px, 29px 167px, 0px 91px);
+        }
+
+        #contraceptives {
+            position: absolute;
+            top: 8px;
+            left: 621px;
+            width: 351px;
+            height: 167px;
+            -webkit-clip-path:polygon(28px 0, 351px 0, 351px 91px, 272px 167px, 0px 167px, 28px 91px);
+        }
+
+        #controlled {
+            position: absolute;
+            top: 170px;
+            left: 806px;
+            width: 166px;
+            height: 355px;
+            -webkit-clip-path:polygon(74px 0, 166px 0, 166px 326px, 74px 327px, 0px 355px, 0px 76px);
+        }
+
+        #diabetic {
+            position: absolute;
+            top: 498px;
+            left: 806px;
+            width: 166px;
+            height: 354px;
+            -webkit-clip-path:polygon(74px 0, 166px 0, 166px 318px, 74px 318px, 0px 290px, 0px 30px);
+        }
+
+        #fridge {
+            position: absolute;
+            top: 790px;
+            left: 806px;
+            width: 166px;
+            height: 343px;
+            -webkit-clip-path:polygon(74px 28px, 166px 29px, 166px 343px, 74px 343px, 0px 273px, 0px 0px);
+        }
+
+        #topicals {
+            position: absolute;
+            top: 231px;
+            left: 276px;
+            width: 433px;
+            height: 164px;
+            -webkit-clip-path:polygon(0 0, 433px 0, 433px 91px, 399px 164px, 35px 164px, 0 91px);
+        }
+
+        #suppositories {
+            position: absolute;
+            top: 457px;
+            left: 274px;
+            width: 433px;
+            height: 165px;
+            -webkit-clip-path:polygon(0 0, 433px 0, 433px 91px, 399px 165px, 35px 165px, 0 91px);
+        }
+
+        #inhalants {
+            position: absolute;
+            top: 684px;
+            left: 274px;
+            width: 433px;
+            height: 165px;
+            -webkit-clip-path:polygon(0 0, 433px 0, 433px 91px, 399px 165px, 35px 165px, 0 91px);
+        }
+
+        #patches {
+            position: absolute;
+            top: 910px;
+            left: 276px;
+            width: 433px;
+            height: 165px;
+            -webkit-clip-path:polygon(0 0, 433px 0, 433px 91px, 399px 165px, 35px 165px, 0 91px);
+        }
+    </style>
+</head>
+<body id="body">
+
+<img src="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/Pharmacy-Floorplan.png" width="100%">
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Tablets_A_2_F.php" class="shelf" id="tabAF">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Tablets_G_2_L.php" class="shelf" id="tabGL">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Tablets_M_2_R.php" class="shelf" id="tabMR">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Tablets_S_2_Z.php" class="shelf" id="tabSZ">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Liquids.php" class="shelf" id="liquids">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Birth_Control.php" class="shelf" id="contraceptives">&nbsp;</a>
+
+<a href="#" class="shelf locked" id="controlled">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Diabetic_Supplies.php" class="shelf" id="diabetic">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Refrigerator.php" class="shelf" id="fridge">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Topicals.php" class="shelf" id="topicals">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Suppositories.php" class="shelf" id="suppositories">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Inhalants.php" class="shelf" id="inhalants">&nbsp;</a>
+
+<a href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Patches.php" class="shelf" id="patches">&nbsp;</a>
+
+</body>
+<?php
+if (isset($_SESSION['savedDrugs'])){
+    echo '<button class="submit" id="verify">Verify Selections</button>';
+}
+?>
+<!--Pages: <span id="output"></span>-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+
+    var ARIS = {};
+    var shelves = document.querySelectorAll(".shelf");
+    var lockedShelf = document.querySelectorAll(".locked");
+
+    for (var i = 0; i < shelves.length; i++) {
+
+        console.log(shelves[i]);
+        shelves[i].addEventListener("touchstart", function() {
+            this.style.background = "rgba(69, 183, 224, .5)";
+        }, false);
+
+    }
+
+//    var lockedShelf = document.querySelectorAll(".locked");
+//
+//    for (var i = 0; i < lockedShelf.length; i++) {
+//
+//        lockedShelf[i].addEventListener("click", function(e) {
+//
+//            var element = this;
+//
+//            setTimeout(function(){
+//                element.style.background = "none";
+//            }, 4000);
+//
+//            $confirmationPopOver = $('<div></div>');
+//            $('body').append($confirmationPopOver);
+//            $confirmationPopOver.text("You need permission from the Pharmacist to access this area. Please see your Facilitator.");
+//            $confirmationPopOver.width(625).height(135).css({
+//                backgroundColor: "white",
+//                position: "absolute",
+//                left: "170px",
+//                top: "525px",
+//                fontSize: "38px",
+//                padding: "20px",
+//                textAlign: "center"
+//            }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+//
+//
+//            e.preventDefault();
+//
+//        }, false);
+
+//    }
+  
+
+
+    /*
+     Used for verifying the drug selections made,
+     can only test in ARIS.
+     */
+
+    ARIS.ready = function() {
+      
+      var permission_id = ARIS.cache.idForItemName('Permission');
+      var permission_qty = ARIS.cache.getPlayerItemCount(permission_id);
+      
+       document.getElementById("controlled").onclick = function(e) {
+         
+         if (permission_qty) {
+           
+           window.location.href="https://www.wisc-online.com/ARISE_Files/PharmTechCustomization/HFPT1A/HFPT1A_Narcotic_Safe.php";
+           
+         } else {
+           
+           e.preventDefault();
+            var element = this;
+
+            setTimeout(function(){
+                element.style.background = "none";
+            }, 4000);
+
+            $confirmationPopOver = $('<div></div>');
+            $('#body').append($confirmationPopOver);
+            $confirmationPopOver.text("You need permission from the Pharmacist to access this area. Please see your Facilitator.");
+            $confirmationPopOver.width(625).height(135).css({
+                backgroundColor: "white",
+                position: "absolute",
+                left: "170px",
+                top: "525px",
+                fontSize: "38px",
+                padding: "20px",
+                textAlign: "center"
+            }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+           
+         }
+         
+
+        };
+      
+      
+        document.getElementById('verify').onclick = function (e) {
+
+            e.preventDefault();
+
+            var list = [];
+            list = <?php echo json_encode($_SESSION['savedDrugs']); ?>;
+            console.log(list);
+
+            var test = false;
+            
+//            var output = document.getElementById("output");
+//            output.innerHTML = "a2fTabs: " + list.a2fTabs + "\ng2lTabs: " + list.g2lTabs + "\nm2rTabs: " + list.m2rTabs + "\ns2zTabs: " + list.s2zTabs + "\ndiabeticSupplies: " + list.diabeticSupplies + "\nrefrigerator: " + list.refrigerator + "\ninhalants: " + list.inhalants;
+                            
+            if (list.a2fTabs == true && list.g2lTabs == true && list.m2rTabs == true && list.s2zTabs == true && list.diabeticSupplies == true && list.refrigerator == true && list.inhalants == true && list.topicals == true && list.narcoticSafe == true) {
+                test = true;
+            }
+
+            if (test) {
+
+                $confirmationPopOver = $('<div></div>');
+                $('#body').append($confirmationPopOver);
+                $confirmationPopOver.text("The ARISE Pharmacist has verified your order is correct. You will progress automatically in a moment.");
+                $confirmationPopOver.width(625).height(125).css({
+                    backgroundColor: "white",
+                    position: "absolute",
+                    left: "170px",
+                    top: "525px",
+                    fontSize: "38px",
+                    padding: "20px",
+                    textAlign: "center"
+                }).hide().fadeIn(1500).delay(2000).fadeOut(3000, function() {
+
+                    var passed = ARIS.cache.idForItemName('passed');
+                    ARIS.setItemCount(passed, 1);
+                    ARIS.exit();
+
+                });
+
+
+            } else {
+
+                $incorrectPopOver = $('<div></div>');
+                $('#body').append($incorrectPopOver);
+                $incorrectPopOver.text("One or more of your selections was incorrect, please try again.");
+                $incorrectPopOver.width(625).height(125).css({
+                    backgroundColor: "white",
+                    position: "absolute",
+                    left: "170px",
+                    top: "525px",
+                    fontSize: "38px",
+                    padding: "20px",
+                    textAlign: "center"
+                }).hide().fadeIn(1500).delay(2000).fadeOut(3000);
+
+            }
+
+
+        };              
+
+    };
+</script>
+</html>
